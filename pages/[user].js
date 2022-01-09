8825096550d04df92cafe32718698a6ac84eed17
JoBess7 <jonathan.bessette.1@ulaval.ca>
2022-01-08 20:38:40 -0500

    using promises.all instead

--- pages/[user].js
+++ pages/[user].js
@@ -14,12 +14,9 @@ export default function User() {
     limitRate: {remaining: null, limit: null},
   });
 
-  const [loaded1, setLoaded1] = useState(false);
-  const [loaded2, setLoaded2] = useState(false);
-  const [loaded3, setLoaded3] = useState(false);
-  const [loaded4, setLoaded4] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
-  function getUserInfo(user) {
+  const getUserInfo = (user) => {
       fetch(`https://api.github.com/users/${user}`)
       .then(response => {
           if(response.status === 404) return setError({active: true, message: ""});
@@ -35,15 +32,13 @@ export default function User() {
                 limit: data.limitRate.limit
             }
           });
-
-          setLoaded1(true);
       })
       .catch(error => {
           setError({active: true, message: "error in getuserinfo"});
       });
-  }
+  };
 
-  function getLimitRate() {
+  const getLimitRate = () => {
       fetch(`https://api.github.com/rate_limit`)
       .then(response => {
           if(response.status === 404) return setError({active: true, message: ""});
@@ -52,7 +47,6 @@ export default function User() {
           return response.json();
       })
       .then(json => {
-          console.log("limit rate ok");
           setData({
             graph: data.graph,
             limitRate: {
@@ -60,24 +54,29 @@ export default function User() {
                 limit: json.rate.limit
             }
           });
-
-          setLoaded2(true);
       })
       .catch(error => {
           setError({active: true, message: "error in getlimitrate"});
       });
-  }
+  };
 
   useEffect(() => {
     const user = router.query.id;
 
     if(user) {
-        getUserInfo(user);
-        getLimitRate();
+        Promise.all([getLimitRate(), getUserInfo(user)])
+        .then(
+          setLoaded(true)
+        )
+        .catch(error => {
+          console.log(error)
+        });
     }
   }, [router.query.id]);
 
   const Render = () => {
+    console.log("loaded: " + loaded);
+    console.log("data: " + data.graph);
     if (error.active) {
       return (
         <div>
@@ -85,7 +84,7 @@ export default function User() {
           <Error message={error.message}/>
         </div>
       );
-    } else if (!loaded1 || !loaded2) {
+    } else if (!loaded) {
       return (
         <div>
           <BackButton/>
